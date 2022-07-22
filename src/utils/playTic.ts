cd2beb1ebbedf129363dcdd0efb636c4fbed3d67
Abigail Dustin <abigail.dustin.21@gmail.com>
2022-07-22 19:59:05 +0300

    update player and sound format

--- src/utils/playTic.ts
+++ src/utils/playTic.ts
@@ -1,5 +1,13 @@
-const tick = new Audio('/assets/tic.wav')
-tick.loop = true
+import { Howl } from "howler";
+// const tick = new Audio('/assets/tic.wav')
+// tick.loop = true
+const tick = new Howl({
+	src: ['/assets/tic.mp3'],
+	loop: true
+}
+)
+
+
 
 export const playTick = (play: boolean)=>{
 	if(!play) tick.pause()
